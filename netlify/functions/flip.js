7a103aadaf6d261de5164c79b37b1f0cb68a8170
rizz <ganzz808@gmail.com>
2025-10-30 16:24:40 +0700

    d

--- netlify/functions/flip.js
+++ netlify/functions/flip.js
@@ -22,9 +22,10 @@ if (RELAYER_PRIVATE_KEY) {
 
 // ABIs
 const USDC_ABI = [
+    'function receiveWithAuthorization(address from, address to, uint256 value, uint256 validAfter, uint256 validBefore, bytes32 nonce, bytes signature) external',
     'function transferWithAuthorization(address from, address to, uint256 value, uint256 validAfter, uint256 validBefore, bytes32 nonce, uint8 v, bytes32 r, bytes32 s) external',
     'function balanceOf(address account) view returns (uint256)',
-    'function transfer(address to, uint256 amount) external returns (bool)' 
+    'event Transfer(address indexed from, address indexed to, uint256 value)'
 ];
 
 const processedAuthorizations = new Set();
